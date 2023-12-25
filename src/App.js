b70cb09a3f724af709588a61c93540ccd560edb2
D S <ds@Ds-MacBook-Pro.local>
2023-12-25 15:11:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+class MarkdownRender extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          textareaValue: "This is a React Markdown. Whatever is typed in the editor will appear in the preview as well. You can delete these texts and put in your own. Thanks for checking by. #W-I-P.",
+      };
+  }
+
+  markdownChange() {
+      const updatedValue = document.getElementById('markdownInput').value;
+      this.setState({
+          textareaValue: updatedValue,
+      });
+
+      // Render the updated value to the output div
+      this.renderToOutput(updatedValue);
+  }
+
+  // New method to render the updated value to the output div
+  renderToOutput(value) {
+      const outputContentElement = document.getElementById('outputContent');
+      if (outputContentElement) {
+          outputContentElement.textContent = `${value}`;
+      }
+  }
+
+  render() {
+      return (
+          <div className='App'>
+            <img src={logo} className="App-logo" alt="logo" />
+      <p>
+         This app was built using React!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              <section className="editor">
+                  <div className="section-title">
+                      <h2><i className="fa fa-pen"></i>Editor</h2>
+                      <i className="fa fa-arrows-alt"></i>
+                  </div>
+                  
+                  <textarea
+                      id="markdownInput"
+                      value={this.state.textareaValue}
+                      onChange={() => this.markdownChange()}
+                  ></textarea>
+              </section>
+              <section className="previewer">
+                  <div className="section-title">
+                      <h2><i className="fa fa-eye"></i>Previewer</h2>
+                      <i className="fa fa-arrows-alt"></i>
+                  </div>
+                  <div id="output" className="preview-output">
+                      
+                      <textarea id='outputContent' value={this.state.textareaValue} readOnly>
+                        
+                      </textarea>
+                  </div>
+              </section>
+             
+              
+          </div>
+      );
+  }
 }
 
-export default App;
+
+
+
+
+export default MarkdownRender ;
